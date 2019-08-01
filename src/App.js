728cd25a87f37fd1a7113db757cb3a794f2c2b70
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-02 02:03:04 +0530

    Add Loading Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Loading from "./components/Loading"
 import Schedule from './components/Schedule';
 
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Schedule />
-      <h3 className="copyright">Made with <font color="red"><b>❤ </b></font> by Injamul Mohammad Mollah</h3>
-     </div>
-  );
+class App extends Component {
+   constructor() {
+     super()
+     this.state = {
+       isLoading: true
+     }
+   }
+   componentDidMount() {
+       setTimeout(() => {
+           this.setState({
+               isLoading: false
+           })
+       }, 1500)
+   }
+
+  render() {
+      return (
+      <div className="App">
+        {this.state.isLoading ?
+        <Loading /> :
+        <div>
+          <Schedule />
+          <h3 className="copyright">Made with <font color="red"><b>❤ </b></font> by Injamul Mohammad Mollah</h3>
+        </div>
+        }
+       </div>
+    );
+  }
 }
 
 export default App;
