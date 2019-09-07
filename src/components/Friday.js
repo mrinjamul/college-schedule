0aaaabcef28fc3c857acd407b1d09c9a05bf6de9
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-09-07 23:42:29 +0530

    add day components

---
+++ src/components/Friday.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Friday() {
+    return(
+        <div>
+            <h1>Friday: Today is Holiday</h1>
+        </div>
+    );
+}
+export default Friday
---
+++ src/components/Saturday.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Saturday() {
+    return(
+        <div>
+            <h1>Saturday: Today is Holiday</h1>
+        </div>
+    );
+}
+export default Saturday
