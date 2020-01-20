93fecdb332903efe6ec0ca4adb6091014dc5fb86
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-01-20 22:24:39 +0530

    updated update-date

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
         <Loading /> :
         <div>
           <Schedule />
-            <h3 className="copyright"> UPDATED ON 1ST AUGUST 2019. </h3> 
+            <h3 className="copyright"> Updated on 20th January 2020. </h3> 
             <h3 className="copyright">Made with <font color="red"><b>❤ </b></font> by Injamul Mohammad Mollah</h3>
         </div>
         }
